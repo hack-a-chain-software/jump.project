2818ad849a1718277e5986a68a4a9bdb9a475b2b
LFSCamargo <luizepauloxd@gmail.com>
2022-05-25 21:39:53 -0300

    fix(front-end): fixes vite build after contract changes

--- packages/web/src/pages/home.tsx
+++ packages/web/src/pages/home.tsx
@@ -1,12 +1,10 @@
-import { WalletConnection } from "near-api-js";
-import { useEffect, useMemo, useState } from "react";
+import { useMemo, useState } from "react";
 import { useNearUser, useNearWallet } from "react-near";
 import { LoadingLottie } from "../assets/animations";
 import { Button } from "../components/button";
 import { If } from "../components/if";
 import { NearLogo } from "../components/nearLogo";
 import { contractName } from "../env/contract";
-import { useGreetingStore } from "../stores/greeting";
 
 /**
  * @route - '/'
@@ -17,23 +15,6 @@ export function Home() {
   const [newGreetingForm, setNewGreetingForm] = useState("");
   const wallet = useNearWallet();
   const user = useNearUser(contractName);
-  const {
-    initializeContract,
-    greeting,
-    setNewGreeting,
-    fetchGreeting,
-    isUpdating,
-  } = useGreetingStore();
-
-  useEffect(() => {
-    if (user.account && user.isConnected)
-      initializeContract(
-        wallet as WalletConnection,
-        user.account?.accountId as string
-      ).then(() => {
-        fetchGreeting(user.account?.accountId as string);
-      });
-  }, [user.isConnected]);
 
   const connectToNear = async () => {
     await wallet?.requestSignIn();
@@ -41,16 +22,6 @@ export function Home() {
   };
 
   const walletConnected = useMemo(() => !!wallet?.isSignedIn(), [wallet]);
-
-  const submit = async () => {
-    if (isUpdating && user.account?.accountId) {
-      return;
-    }
-
-    await setNewGreeting(newGreetingForm, user.account?.accountId as string);
-
-    setNewGreetingForm("");
-  };
 
   return (
     <div className="p-4 flex min-h-[70vh] flex-1 flex-col items-center justify-center">
@@ -82,43 +53,19 @@ export function Home() {
           condition={walletConnected}
         >
           <div className="overflow-hidden">
-            <If
-              fallback={
-                <h1 className="h-auto text-center font-[800] text-4xl tracking-[-0.06em]  overflow-hidden delay-75 w3-animate-bottom">
-                  Changing Greeting...
-                </h1>
-              }
-              condition={!isUpdating}
-            >
-              <h1 className="h-auto text-center font-[800] text-4xl tracking-[-0.06em]  overflow-hidden delay-75">
-                <strong className="w3-animate-bottom mr-2">
-                  {greeting || "Hello"}
-                </strong>
-                {user.account?.accountId}
-              </h1>
-            </If>
+            <h1 className="h-auto text-center font-[800] text-4xl tracking-[-0.06em]  overflow-hidden delay-75">
+              <strong className="mr-2 w3-animate-bottom">Hello</strong>
+              {user.account?.accountId}
+            </h1>
           </div>
           <div className="overflow-hidden">
             <h1 className="h-auto text-center font-[800] text-3xl tracking-[-0.06em] w3-animate-bottom overflow-hidden">
-              Welcome to the Near Monorepo
+              Welcome to the Jump Project on NEAR Protocol
             </h1>
           </div>
           <h1 className="mt-3 max-w-[600px] text-center font-semibold opacity-[0.6] text-xl tracking-[-0.06em] w3-animate-fading-in">
-            A Monorepo that helps you building Dapps on NEAR in the right way,
-            Interact with the sample contract that works this way the user can
-            configure a greeting on the website for him so go ahead!
+            More Coming Soon
           </h1>
-          <div className="flex gap-2 mt-4 flex-row items-center">
-            <input
-              value={newGreetingForm}
-              onChange={(e) => setNewGreetingForm(e.target.value)}
-              placeholder="New Greeting"
-              className="input bg-gray-900 border-[2px] border-gray-900 focus:border-white outline-none duration-75 text-white px-5 py-2.5 size-[16px] tracking-[-0.03em] flex rounded-2xl"
-            />
-            <Button loading={isUpdating} onClick={submit}>
-              Update Greeting
-            </Button>
-          </div>
         </If>
       </If>
     </div>
