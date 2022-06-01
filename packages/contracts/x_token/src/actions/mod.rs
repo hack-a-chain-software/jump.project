71d7520fff5dfafe5350598514840905cf932ad7
veigajoao <veigajoaoa@gmail.com>
2022-06-01 14:38:57 -0300

    add view function

--- packages/contracts/x_token/src/actions/mod.rs
+++ packages/contracts/x_token/src/actions/mod.rs
@@ -1,3 +1,4 @@
 pub mod user_actions;
 pub mod callback_actions;
 pub mod nep_actions;
+pub mod view_actions;
---
+++ packages/contracts/x_token/src/actions/view_actions.rs
@@ -0,0 +1,13 @@
+use crate::*;
+use std::collections::{HashMap};
+
+#[near_bindgen]
+impl Contract {
+    pub fn view_token_ratio(&self) -> HashMap<String, U128> {
+        let return_value: HashMap<String, U128> = HashMap::new();
+        return_value.insert("x_token".to_string(), U128(self.ft_functionality.total_supply));
+        return_value.insert("base_token".to_string(), U128(self.base_token_treasury));
+        return_value
+    }
+
+}
