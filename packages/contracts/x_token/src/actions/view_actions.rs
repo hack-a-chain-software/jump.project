c6851c480fd44a22a49fd8b744a234537fab9268
veigajoao <veigajoaoa@gmail.com>
2022-06-01 14:40:06 -0300

    add view call

--- packages/contracts/x_token/src/actions/view_actions.rs
+++ packages/contracts/x_token/src/actions/view_actions.rs
@@ -6,7 +6,7 @@ impl Contract {
     pub fn view_token_ratio(&self) -> HashMap<String, U128> {
         let return_value: HashMap<String, U128> = HashMap::new();
         return_value.insert("x_token".to_string(), U128(self.ft_functionality.total_supply));
-        return_value.insert("base_token".to_string(), U128(self.base_token_treasury));
+        return_value.insert(self.base_token.to_string(), U128(self.base_token_treasury));
         return_value
     }
 
