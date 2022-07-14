4b013a35d2716fd06d1f990f5a41329ee87acdb1
Jasso <veigajoaoa@gmail.com>
2022-07-14 11:08:01 -0300

    add withdraw test

--- packages/contracts/locked_jump/src/actions/owner.rs
+++ packages/contracts/locked_jump/src/actions/owner.rs
@@ -17,3 +17,74 @@ impl Contract {
         self.contract_config.set(&new_config);
     }
 }
+
+#[cfg(test)]
+mod tests {
+
+    // use crate::tests::*;
+
+    // /// assign_guardian
+    // /// Method must:
+    // /// 1. Assert caller is owner
+    // /// 2. Assert 1 yocto near was deposited
+    // /// 3. Add new guardian to set
+    // /// 4. emit new guardian event
+    // #[test]
+    // fn test_assign_guardian() {
+    //   fn closure_generator(caller: AccountId, deposit: u128, seed: u128) -> impl FnOnce() {
+    //     move || {
+    //       testing_env!(get_context(
+    //         vec![],
+    //         deposit,
+    //         0,
+    //         caller,
+    //         0,
+    //         Gas(300u64 * 10u64.pow(12)),
+    //       ));
+  
+    //       let guardian_account: AccountId = USER_ACCOUNT.parse().unwrap();
+  
+    //       let mut contract = init_contract(seed);
+  
+    //       assert!(!contract.guardians.contains(&guardian_account));
+    //       contract.assign_guardian(guardian_account.clone());
+    //       assert!(contract.guardians.contains(&guardian_account));
+    //       let logs = get_logs();
+    //       assert_eq!(logs.len(), 1);
+  
+    //       let event_log = logs.get(0).unwrap();
+    //       let serde_blob: serde_json::Value =
+    //         serde_json::from_str(event_log.chars().skip(11).collect::<String>().as_str()).unwrap();
+  
+    //       assert_eq!(serde_blob["standard"], "jump_launchpad");
+    //       assert_eq!(serde_blob["version"], "1.0.0");
+    //       assert_eq!(serde_blob["event"], "create_guardian");
+    //       assert_eq!(
+    //         serde_blob["data"][0]["new_guardian"],
+    //         guardian_account.to_string()
+    //       );
+    //     }
+    //   }
+  
+    //   let test_cases = [
+    //     // 1. Assert caller is owner or guardian
+    //     (USER_ACCOUNT.parse().unwrap(), 1, Some(ERR_001.to_string())),
+    //     // 2. Assert 1 yocto near was deposited
+    //     (
+    //       OWNER_ACCOUNT.parse().unwrap(),
+    //       0,
+    //       Some("Requires attached deposit of exactly 1 yoctoNEAR".to_string()),
+    //     ),
+    //     // 3. Add new guardian to set
+    //     // 4. emit new guardian event
+    //     (OWNER_ACCOUNT.parse().unwrap(), 1, None),
+    //   ];
+  
+    //   let mut counter = 0;
+    //   IntoIterator::into_iter(test_cases).for_each(|v| {
+    //     run_test_case(closure_generator(v.0, v.1, counter), v.2);
+    //     counter += 1;
+    //   });
+    // }
+
+}
