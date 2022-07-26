920e0af855c988bb8021e033176ab3f29f3fbaf5
Mateus Santana <42101423+1Mateus@users.noreply.github.com>
2022-07-26 15:09:39 -0300

    feat: update

--- packages/ts/src/helpers/types.ts
+++ packages/ts/src/helpers/types.ts
@@ -5,3 +5,17 @@ export type WithNearParams<Params = any> = {
 };
 
 export type Option<T> = T | null;
+
+export type U128 = string;
+
+/** @description - This is a near mutable contract call generic type for the `Contract` interface */
+export type NearMutableContractCall<Params extends Record<string, any>> = (
+  params: Params,
+  gas: string,
+  deposit: string
+) => Promise<void>;
+
+export type NearContractViewCall<
+  Params extends Record<string, any>,
+  Data extends Record<string, any>
+> = (params: Params) => Promise<Data>;
