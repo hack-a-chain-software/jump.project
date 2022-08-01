9e158a2b686bd57a21f63b1bdaffdab27ed93442
Mateus Santana <42101423+1Mateus@users.noreply.github.com>
2022-08-01 20:53:40 -0300

    Update index.ts

--- packages/graphql/src/env/index.ts
+++ packages/graphql/src/env/index.ts
@@ -3,12 +3,12 @@ import { config } from "dotenv";
 config();
 
 export const EnvVariables = {
-  db_host: process.env.DB_HOST,
-  db_name: process.env.DB_NAME,
+  db_host: process.env.DB_HOST || "",
   db_port: process.env.DB_PORT || 5432,
-  server_port: process.env.SERVER_PORT || 4000,
   db_password: process.env.DB_PASSWORD || "",
+  db_name: process.env.DB_NAME || "jdx_dev_db",
+  server_port: process.env.SERVER_PORT || 4000,
   db_dialect: process.env.DB_DIALECT || "postgres",
   db_username: process.env.DB_USERNAME || "postgres",
-  rpcURL: process.env.NEAR_RPC_URL || "https://rpc.testnet.near.org",
+  rpc_url: process.env.NEAR_RPC_URL || "https://rpc.testnet.near.org",
 };
