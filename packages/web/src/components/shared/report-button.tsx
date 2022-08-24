c1d6a77fd8774ae4934085fce7196ccbef07e840
Jasso <veigajoaoa@gmail.com>
2022-08-24 16:35:58 -0300

    feat(web): add feedback button

--- packages/web/src/components/shared/report-button.tsx
+++ packages/web/src/components/shared/report-button.tsx
@@ -1,5 +1,5 @@
 import { useTheme } from "../../hooks/theme";
-import { ExclamationIcon } from "@heroicons/react/solid";
+import { ExclamationIcon, ClipboardListIcon } from "@heroicons/react/solid";
 import { Flex, useColorModeValue } from "@chakra-ui/react";
 
 export function ReportButton() {
@@ -9,21 +9,21 @@ export function ReportButton() {
   const cardOpacity = useColorModeValue(glassyWhiteOpaque, glassyWhiteOpaque);
 
   return (
-    <Flex
-      bg={cardBg}
-      onClick={() => {
-        window.open(
-          "https://docs.google.com/forms/d/e/1FAIpQLSdFwl4C7HKeYZxfgeZu62BakWBHBM9-WihDMJ4w2Rl2nMcV_Q/viewform",
-          "_blank"
-        );
-      }}
-      borderRadius="24px"
-      className="fixed bottom-[24px] right-[24px] z-[9999] cursor-pointer hover:opacity-[0.95]"
-    >
+    <Flex className="fixed bottom-[24px] right-[24px] z-[9999] cursor-pointer hover:opacity-[0.95]">
       <Flex
+        bg={cardBg}
+        onClick={() => {
+          window.open(
+            "https://docs.google.com/forms/d/e/1FAIpQLSemWrQk223Uf0OsSrJ-lfuAZmd2CWrC9okmduxA_MLZO0HcvQ/viewform",
+            "_blank"
+          );
+        }}
         borderRadius="24px"
-        bg={cardOpacity}
-        className="
+      >
+        <Flex
+          borderRadius="24px"
+          bg={cardOpacity}
+          className="
           w-[180px] 
           flex items-center
           space-x-[12px]
@@ -31,10 +31,39 @@ export function ReportButton() {
           py-[9px]
           text-white
         "
-      >
-        <ExclamationIcon className="w-8" />
+        >
+          <ClipboardListIcon className="w-8" />
 
-        <span className="text-[18px]">Report Bug</span>
+          <span className="text-[18px]">Feedback</span>
+        </Flex>
+      </Flex>
+
+      <Flex
+        bg={cardBg}
+        onClick={() => {
+          window.open(
+            "https://docs.google.com/forms/d/e/1FAIpQLSdFwl4C7HKeYZxfgeZu62BakWBHBM9-WihDMJ4w2Rl2nMcV_Q/viewform",
+            "_blank"
+          );
+        }}
+        borderRadius="24px"
+      >
+        <Flex
+          borderRadius="24px"
+          bg={cardOpacity}
+          className="
+          w-[180px] 
+          flex items-center
+          space-x-[12px]
+          px-[16px]
+          py-[9px]
+          text-white
+        "
+        >
+          <ClipboardListIcon className="w-8" />
+
+          <span className="text-[18px]">Report Bug</span>
+        </Flex>
       </Flex>
     </Flex>
   );
