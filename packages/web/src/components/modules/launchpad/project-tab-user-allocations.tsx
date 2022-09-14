161f67ab00a5a84c617cc1ece1c97feb9d667d6b
Antonio Moreno <amrh07@gmail.com>
2022-09-14 19:27:10 -0300

    feat(web): changed tabs color and changed the position of the button in the user section

--- packages/web/src/components/modules/launchpad/project-tab-user-allocations.tsx
+++ packages/web/src/components/modules/launchpad/project-tab-user-allocations.tsx
@@ -15,13 +15,13 @@ export function ProjectTabUserAllocations({
   return (
     <Card className="col-span-12 lg:col-span-6 xl:col-span-4 relative project-allocations">
       <Flex direction="column" justify="flex-start" height="100%">
-        <Tabs variant="soft-rounded" colorScheme="whiteAlpha">
+        <Tabs variant="soft-rounded" colorScheme="gray" className="h-full">
           <TabList>
             <Tab>User Area</Tab>
             <Tab>Join Project</Tab>
           </TabList>
-          <TabPanels>
-            <TabPanel>{projectUserComponent}</TabPanel>
+          <TabPanels className="h-full">
+            <TabPanel className="h-full">{projectUserComponent}</TabPanel>
             <TabPanel>{projectAllocationsComponent}</TabPanel>
           </TabPanels>
         </Tabs>
--- packages/web/src/components/modules/launchpad/project-user-area.tsx
+++ packages/web/src/components/modules/launchpad/project-user-area.tsx
@@ -128,95 +128,96 @@ export function ProjectUserArea({
         }}
       />
 
-      <Flex className="flex-col space-y-[12px] h-full w-full">
-        <GradientText fontWeight="800" letterSpacing="-0,03em" fontSize={24}>
-          User Area
-        </GradientText>
+      <Flex className="flex-col space-y-[12px] h-5/6 w-full">
+        <Flex className="flex-col space-y-[12px] justify-evenly h-5/6 w-full">
+          <GradientText fontWeight="800" letterSpacing="-0,03em" fontSize={24}>
+            User Area
+          </GradientText>
 
-        <div className="flex-col space-y-[4px]">
-          <Skeleton
-            isLoaded={!isLoading}
-            className="flex space-x-[4px] rounded-[16px]"
-          >
-            <div>
-              <span className="text-[18px] font-[600] tracking-[-0.05em]">
-                Allocations:
-              </span>
-            </div>
+          <div className="flex-col space-y-[4px]">
+            <Skeleton
+              isLoaded={!isLoading}
+              className="flex space-x-[4px] rounded-[16px]"
+            >
+              <div>
+                <span className="text-[18px] font-[600] tracking-[-0.05em]">
+                  Allocations:
+                </span>
+              </div>
 
-            <div>
-              <span className="text-[18px] font-[500] tracking-[-0.05em]">
-                {accountId
-                  ? investorAllocation.allocationsBought ?? "0"
-                  : CONNECT_WALLET_MESSAGE}
-              </span>
-            </div>
-          </Skeleton>
+              <div>
+                <span className="text-[18px] font-[500] tracking-[-0.05em]">
+                  {accountId
+                    ? investorAllocation.allocationsBought ?? "0"
+                    : CONNECT_WALLET_MESSAGE}
+                </span>
+              </div>
+            </Skeleton>
 
-          <Skeleton
-            isLoaded={!isLoading}
-            className="flex space-x-[4px] rounded-[16px]"
-          >
-            <div>
-              <span className="text-[18px] font-[600] tracking-[-0.05em]">
-                Total amount:
-              </span>
-            </div>
+            <Skeleton
+              isLoaded={!isLoading}
+              className="flex space-x-[4px] rounded-[16px]"
+            >
+              <div>
+                <span className="text-[18px] font-[600] tracking-[-0.05em]">
+                  Total amount:
+                </span>
+              </div>
 
-            <div>
-              <span className="text-[18px] font-[500] tracking-[-0.05em]">
-                {accountId ? totalAmount : CONNECT_WALLET_MESSAGE}
-              </span>
-            </div>
-          </Skeleton>
+              <div>
+                <span className="text-[18px] font-[500] tracking-[-0.05em]">
+                  {accountId ? totalAmount : CONNECT_WALLET_MESSAGE}
+                </span>
+              </div>
+            </Skeleton>
 
-          <Skeleton
-            isLoaded={!isLoading}
-            className="flex space-x-[4px] rounded-[16px]"
-          >
-            <div>
-              <span className="text-[18px] font-[600] tracking-[-0.05em]">
-                Unlocked amount:
-              </span>
-            </div>
+            <Skeleton
+              isLoaded={!isLoading}
+              className="flex space-x-[4px] rounded-[16px]"
+            >
+              <div>
+                <span className="text-[18px] font-[600] tracking-[-0.05em]">
+                  Unlocked amount:
+                </span>
+              </div>
 
-            <div>
-              <span className="text-[18px] font-[500] tracking-[-0.05em]">
-                {accountId
-                  ? formatNumber(
-                      unlockedAmount,
-                      decimals,
-                      metadataProjectToken?.symbol!
-                    )
-                  : CONNECT_WALLET_MESSAGE}
-              </span>
-            </div>
-          </Skeleton>
+              <div>
+                <span className="text-[18px] font-[500] tracking-[-0.05em]">
+                  {accountId
+                    ? formatNumber(
+                        unlockedAmount,
+                        decimals,
+                        metadataProjectToken?.symbol!
+                      )
+                    : CONNECT_WALLET_MESSAGE}
+                </span>
+              </div>
+            </Skeleton>
 
-          <Skeleton
-            isLoaded={!isLoading}
-            className="flex space-x-[4px] rounded-[16px]"
-          >
-            <div>
-              <span className="text-[18px] font-[600] tracking-[-0.05em]">
-                Claimed amount:
-              </span>
-            </div>
+            <Skeleton
+              isLoaded={!isLoading}
+              className="flex space-x-[4px] rounded-[16px]"
+            >
+              <div>
+                <span className="text-[18px] font-[600] tracking-[-0.05em]">
+                  Claimed amount:
+                </span>
+              </div>
 
-            <div>
-              <span className="text-[18px] font-[500] tracking-[-0.05em]">
-                {accountId
-                  ? formatNumber(
-                      claimedAmount,
-                      decimals,
-                      metadataProjectToken?.symbol!
-                    )
-                  : CONNECT_WALLET_MESSAGE}
-              </span>
-            </div>
-          </Skeleton>
-        </div>
-
+              <div>
+                <span className="text-[18px] font-[500] tracking-[-0.05em]">
+                  {accountId
+                    ? formatNumber(
+                        claimedAmount,
+                        decimals,
+                        metadataProjectToken?.symbol!
+                      )
+                    : CONNECT_WALLET_MESSAGE}
+                </span>
+              </div>
+            </Skeleton>
+          </div>
+        </Flex>
         <Skeleton
           isLoaded={!isLoading}
           w="100%"
