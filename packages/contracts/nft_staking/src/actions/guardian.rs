033329b2a0bc0e807c2aa91c39a13860f6b7ebb1
Eduardo <eduardogbg@gmail.com>
2022-09-23 07:46:49 -0300

    refac: integrate deposits to treasury operations

--- packages/contracts/nft_staking/src/actions/guardian.rs
+++ packages/contracts/nft_staking/src/actions/guardian.rs
@@ -98,12 +98,11 @@ impl Contract {
     assert_one_yocto();
 
     self.move_treasury(
-      TreasuryOperation::ContractToCollection {
-        amount: Some(amount.0),
-      },
+      TreasuryOperation::ContractToCollection,
       &env::predecessor_account_id(),
       &collection,
       token_id,
+      Some(amount.0),
     );
   }
 
@@ -117,12 +116,11 @@ impl Contract {
     assert_one_yocto();
 
     self.move_treasury(
-      TreasuryOperation::CollectionToContract {
-        amount: Some(amount.0),
-      },
+      TreasuryOperation::CollectionToContract,
       &env::predecessor_account_id(),
       &collection,
       token_id,
+      Some(amount.0),
     );
   }
 }
