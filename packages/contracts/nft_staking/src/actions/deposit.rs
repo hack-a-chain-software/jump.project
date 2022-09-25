30aace9371adaf851661132bb9158299e6701d99
Jasso <veigajoaoa@gmail.com>
2022-09-25 08:57:41 -0300

    fix(nft-staking): add route to deposit contract_tokens

--- packages/contracts/nft_staking/src/actions/deposit.rs
+++ packages/contracts/nft_staking/src/actions/deposit.rs
@@ -1,3 +1,4 @@
+use near_sdk::env;
 use crate::{
   actions::transfer::FTRoutePayload, treasury::TreasuryOperation, types::NFTCollection, Contract,
 };
@@ -8,6 +9,17 @@ impl Contract {
       TreasuryOperation::DepositToDistribution,
       &payload.sender_id,
       &collection,
+      payload.token_id,
+      Some(payload.amount),
+    );
+  }
+
+  pub fn deposit_contract_funds(&mut self, payload: FTRoutePayload) {
+    self.move_treasury(
+      TreasuryOperation::DepositToContract,
+      &payload.sender_id,
+      // pass dummy collection as it is not relevant for contract_funds deposit
+      &NFTCollection::NFTContract { account_id: env::current_account_id() },
       payload.token_id,
       Some(payload.amount),
     );
