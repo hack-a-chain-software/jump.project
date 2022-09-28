6db11ee640d92611d93a2c6ca395cdfc1b08b0cf
Eduardo <eduardogbg@gmail.com>
2022-09-28 09:17:44 -0300

    fix(nft-staking): fix staked NFT withdraw

--- packages/contracts/nft_staking/src/models/staked_nft.rs
+++ packages/contracts/nft_staking/src/models/staked_nft.rs
@@ -34,12 +34,77 @@ impl StakedNFT {
   }
 
   pub fn withdraw(&mut self) -> FungibleTokenBalance {
-    let withdrawn_balance = HashMap::new().clone();
+    let withdrawn_balance = self.balance.clone();
 
     for (_, v) in self.balance.iter_mut() {
       *v = 0;
     }
 
     withdrawn_balance
+  }
+}
+
+#[cfg(test)]
+mod tests {
+  use std::str::FromStr;
+
+  use near_sdk::AccountId;
+  use rstest::{fixture, rstest};
+
+  use crate::types::{FungibleTokenID, NFTCollection};
+
+  use super::*;
+
+  #[fixture]
+  pub fn contract_token() -> FungibleTokenID {
+    AccountId::from_str("contract_token.near").unwrap()
+  }
+
+  #[fixture]
+  pub fn program_token() -> FungibleTokenID {
+    AccountId::from_str("program_token.near").unwrap()
+  }
+
+  #[fixture]
+  fn collection() -> NFTCollection {
+    NFTCollection::NFTContract {
+      account_id: AccountId::from_str("collection.near").unwrap(),
+    }
+  }
+
+  #[fixture]
+  fn staker() -> AccountId {
+    AccountId::from_str("staker.near").unwrap()
+  }
+
+  #[fixture]
+  fn staked_nft(
+    collection: NFTCollection,
+    staker: AccountId,
+    contract_token: FungibleTokenID,
+    program_token: FungibleTokenID,
+  ) -> StakedNFT {
+    let id = (collection, "#1".to_string());
+    let mut staked_nft = StakedNFT::new(id, staker, 5 * 10u64.pow(9));
+
+    staked_nft.balance.insert(contract_token, 10);
+    staked_nft.balance.insert(program_token, 5);
+
+    staked_nft
+  }
+
+  #[rstest]
+  fn test_withdraw(mut staked_nft: StakedNFT) {
+    let initial_balance = staked_nft.balance.clone();
+
+    let withdrawn_balance = staked_nft.withdraw();
+
+    let final_balance = staked_nft.balance;
+
+    assert_eq!(withdrawn_balance, initial_balance);
+
+    for &v in final_balance.values() {
+      assert_eq!(v, 0);
+    }
   }
 }
