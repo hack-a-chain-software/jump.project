3c2c9baa3748cac642af2d179ab4f09a70e79761
Jasso <veigajoaoa@gmail.com>
2022-09-28 19:18:38 -0300

    test ebs deploy 1

--- .github/utilities/set_staging_environments.js
+++ .github/utilities/set_staging_environments.js
@@ -27,6 +27,7 @@ DB_HOST=${process.env.DB_HOST}
 DB_PORT=5432
 DB_NAME=${process.env.DB_NAME}
 NFT_STAKING_CONTRACT=${contracts_data.nftStaking}
+ENV=TESTNET
 `;
 
 fs.writeFileSync("./packages/graphql/.env", environment_file_graphql);
