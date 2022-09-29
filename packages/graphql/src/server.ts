a51a1047acc08c15f48ac1e6e429db31d1757d8c
Jasso <veigajoaoa@gmail.com>
2022-09-29 00:44:42 -0300

    add db envs to repo

--- packages/graphql/src/server.ts
+++ packages/graphql/src/server.ts
@@ -16,7 +16,7 @@ const {
 const sequelize = new Sequelize({
   port: db_port,
   // host: db_host,
-  host: "postgres-jump-testnet.c8fvx3d5adgx.us-east-1.rds.amazonaws.com",
+  host: db_host,
   database: db_name,
   dialect: db_dialect,
   username: db_username,
