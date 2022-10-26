b19b8e56985f1a3034b1cabd4bfaedb932a68576
Eduardo <eduardogbg@gmail.com>
2022-10-26 17:08:36 -0300

    fix(nft-staking): audit issue 7 farm distribution

--- packages/contracts/tests/src/tests_nft_staking/mod.rs
+++ packages/contracts/tests/src/tests_nft_staking/mod.rs
@@ -1,6 +1,6 @@
 #[cfg(test)]
 mod tests {
-  use std::collections::HashMap;
+  use std::{collections::HashMap};
 
   use rstest::rstest;
   use serde_json::json;
@@ -17,8 +17,7 @@ mod tests {
       nft::{initialize_nft_contract, nft_mint, view_nft_token},
     },
     lib::events::{parse_event_logs, find_event_type},
-    create_user_account, get_wasm, deploy_contract, deploy_contract_from_wasm_path, time_travel,
-    register_user,
+    create_user_account, get_wasm, deploy_contract, deploy_contract_from_wasm_path, register_user,
   };
 
   #[derive(Debug, Clone)]
@@ -157,6 +156,8 @@ mod tests {
   #[rstest]
   #[tokio::test]
   async fn test_normal_flow() -> anyhow::Result<()> {
+    let worker = &worker().await;
+
     let fixtures = setup_test().await;
     let accounts = fixtures.accounts;
     let contracts = fixtures.contracts;
@@ -173,18 +174,20 @@ mod tests {
 
     add_guardian(&nft_staking, &owner, guardian.id()).await;
 
+    let contract_token_funds = 600000000000000;
+
     deposit(
       &nft_staking,
       &contract_token,
       &owner,
-      300000000000000,
+      contract_token_funds,
       json!({ "type": "ContractTreasury" }),
     )
     .await;
 
     let mut collection_rps = HashMap::new();
     let contract_reward: u128 = 20000000000000;
-    let program_reward: u128 = 20000000000000;
+    let program_reward: u128 = 30000000000000;
     collection_rps.insert(
       contract_token.as_account().id(),
       format!("{contract_reward}"),
@@ -192,6 +195,8 @@ mod tests {
     collection_rps.insert(program_token.as_account().id(), format!("{program_reward}"));
 
     let early_withdraw_penalty: u128 = 1000000000000;
+    let round_interval_ms: u64 = 1200;
+    let start_in_ms: u64 = 3000;
 
     create_staking_program(
       &nft_staking,
@@ -202,12 +207,15 @@ mod tests {
         "token_address": program_token.as_account().id(),
         "collection_rps": collection_rps,
         "min_staking_period": "20000",
-        "early_withdraw_penalty": "1000000000000",
-        "round_interval": "10000",
-        "start_in": "5000",
+        "early_withdraw_penalty": early_withdraw_penalty.to_string(),
+        "round_interval": round_interval_ms.to_string(),
+        "start_in": start_in_ms.to_string(),
       }),
     )
     .await;
+
+    let initial_timestamp = worker.view_latest_block().await?.timestamp();
+    let start_at_ms = (initial_timestamp / 10_u64.pow(6)) + start_in_ms;
 
     transfer(
       &nft_staking,
@@ -219,7 +227,7 @@ mod tests {
           "account_id": nft_collection.as_account().id(),
         },
         "token_id": contract_token.as_account().id(),
-        "amount": "300000000000000",
+        "amount": contract_token_funds.to_string(),
       }),
     )
     .await;
@@ -234,16 +242,18 @@ mod tests {
           "account_id": nft_collection.as_account().id(),
         },
         "token_id": contract_token.as_account().id(),
-        "amount": "300000000000000",
+        "amount": contract_token_funds.to_string(),
       }),
     )
     .await;
+
+    let program_token_funds = 600000000000000;
 
     deposit(
       &nft_staking,
       &program_token,
       &collection_owner,
-      300000000000000,
+      program_token_funds,
       json!({
         "type": "CollectionTreasury",
         "collection": { "type": "NFTContract", "account_id": &nft_collection.as_account().id() }
@@ -261,7 +271,7 @@ mod tests {
           "account_id": nft_collection.as_account().id(),
         },
         "token_id": program_token.as_account().id(),
-        "amount": "300000000000000",
+        "amount": program_token_funds.to_string(),
       }),
     )
     .await;
@@ -272,9 +282,24 @@ mod tests {
     // TODO: traverse execution outcomes for possible cross-contract calls errors
     stake(&nft_staking, &nft_collection, &staker, nft_id).await;
 
-    time_travel(&worker().await, 20).await?;
+    let stake_timestamp_ms = worker.view_latest_block().await?.timestamp() / 10_u64.pow(6);
+    let stake_round = stake_timestamp_ms.checked_sub(start_at_ms).unwrap_or(0) / round_interval_ms;
+
+    /*
+        For some unknown reason time travel actually makes it difficult to get accurate block timestamps.
+      The behavior I was experiencing while using time-travel is that the block timestamp for the unstaking
+      transaction would be lesser than the latests block timestamp before it, which violates causality and a
+      lot of the invariants assumed in this test.
+    */
+    // time_travel(worker, 6).await?;
 
     unstake(&nft_staking, &nft_collection, &staker, nft_id).await;
+
+    let unstake_timestamp_ms = worker.view_latest_block().await?.timestamp() / 10_u64.pow(6);
+    let unstake_round =
+      unstake_timestamp_ms.checked_sub(start_at_ms).unwrap_or(0) / round_interval_ms;
+
+    let rounds: u128 = (unstake_round - stake_round).into();
 
     let nft = view_nft_token(&nft_collection, nft_id).await?.unwrap();
     assert_eq!(nft.owner_id.as_str(), staker.id().as_str());
@@ -288,7 +313,21 @@ mod tests {
         .unwrap();
       let tokens = u128::from_str_radix(tokens_json.as_str().unwrap(), 10)?;
 
-      assert_eq!(tokens * DENOM, reward * (DENOM - early_withdraw_penalty));
+      let real_reward_per_round = reward * (DENOM - early_withdraw_penalty);
+      let withdrawn_reward = tokens * DENOM;
+
+      /*
+          As we have no way way to get the exact block timestamp, and the test environment is not consistent
+        with the execution time of each transaction, it is possible that our estimated unstake timestamp
+        is actually 1 round higher than the actual one.
+
+          We mitigate the impact of this issue on our test case assertiveness by using a small round interval,
+        so that multiple rounds should pass, ensuring that this inconsistency shouldn't result in false-negatives.
+      */
+      assert!(
+        withdrawn_reward == real_reward_per_round * (rounds - 1)
+          || withdrawn_reward == real_reward_per_round * rounds
+      );
 
       Ok(())
     };
