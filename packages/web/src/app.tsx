054c17bd4be5603de0a0162927d3162a5414dea1
Mateus Santana <42101423+1Mateus@users.noreply.github.com>
2022-11-03 15:18:54 -0300

    feat(front): add toast on actionables

--- packages/web/src/app.tsx
+++ packages/web/src/app.tsx
@@ -3,20 +3,50 @@ import { Toaster } from "react-hot-toast";
 import { Header, Nav, ReportButton } from "./components";
 import { useTheme } from "./hooks/theme";
 import { BrowserRouter as Router, useRoutes } from "react-router-dom";
-
+import { useWalletSelector } from "@/context/wallet-selector";
+import { getTransactionState, getTransactionsAction } from "./tools";
 import routes from "virtual:generated-pages-react";
+import { useEffect } from "react";
+import toast from "react-hot-toast";
 
 const Pages = () => {
   return useRoutes(routes);
 };
 
-/**
- * @name Router
- * @description - This is the application router that will have all the app routes!
- * And also some business logic to handle near initialization
- */
+const transactionHashes = new URLSearchParams(window.location.search).get(
+  "transactionHashes"
+);
+
 function App() {
   const { gradientBackground } = useTheme();
+
+  const { accountId } = useWalletSelector();
+
+  useEffect(() => {
+    if (!accountId || !transactionHashes) {
+      return;
+    }
+
+    (async () => {
+      const transactions = transactionHashes.split(",");
+
+      const states: any[] = [];
+
+      for (let i = 0; i < transactions.length; i++) {
+        const state = await getTransactionState(transactions[i], accountId);
+
+        states.push(state);
+      }
+
+      const action = getTransactionsAction(states);
+
+      if (!action) {
+        return;
+      }
+
+      toast[action.status](action.message);
+    })();
+  }, [accountId]);
 
   return (
     <Flex flex={1} bg={gradientBackground}>
