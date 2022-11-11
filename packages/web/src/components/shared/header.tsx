1b01f92f691bdc7f70a767ed136554d6eeb80651
Lara Vieira <contato@laravieira.me>
2022-11-11 17:30:19 -0300

    fix(web): Implemented new design on header.

--- packages/web/src/components/shared/header.tsx
+++ packages/web/src/components/shared/header.tsx
@@ -1,58 +1,79 @@
-import { Flex, Text, useColorMode } from "@chakra-ui/react";
-import { MoonIcon, MenuIcon } from "../../assets/svg";
-import { JumpTextIcon } from "../../assets/svg/jump-text";
+import { JumpTextIcon } from "@/assets/svg/jump-text";
 import { Button } from "./button";
 import { MobileNav } from "./mobile-nav";
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import { AirdropModal } from "@/modals";
 import { Wallet } from "./wallet";
+import { JumpIcon } from "@/assets/svg/jump-logo";
+import { MoonIcon, Bars3Icon } from "@heroicons/react/24/solid";
+import { SunIcon } from "@heroicons/react/24/outline";
+
+const SCROLL_TOLERANCE = 8;
 
 export function Header() {
-  const { toggleColorMode } = useColorMode();
   const [isOpen, setIsOpen] = useState(false);
   const [showTokenModal, setShowTokenModal] = useState(false);
+  const [isStyleSchemeDark, setStyleScheme] = useState(true);
+  const [isHeaderFloating, setHeaderFloating] = useState(
+    window.scrollY > SCROLL_TOLERANCE
+  );
+
+  useEffect(() => {
+    // Add/remove scrolling listener to isHeaderFloating
+    const onScroll = () => setHeaderFloating(window.scrollY > SCROLL_TOLERANCE);
+
+    window.addEventListener("scroll", onScroll);
+
+    return () => window.removeEventListener("scroll", onScroll);
+  }, []);
+
+  const classes =
+    "transition-all fixed inset-0 bottom-auto z-50 flex items-center justify-between px-8 h-[74px]";
+  const floatingClasses = " shadow-lg backdrop-blur bg-white-600";
 
   return (
-    <div className="z-10 pt-[30px] absolute top-0 right-0 z-2 left-0 flex items-center justify-center md:ml-[120px]">
-      <AirdropModal
-        isOpen={showTokenModal}
-        onClose={() => setShowTokenModal(false)}
-      />
-      <Flex className="flex px-10 pb-8 items-center w-[100%] justify-between">
-        <Flex alignItems="center" className="space-x-[48px]">
-          <Flex className="hidden md:block">
-            <JumpTextIcon />
-          </Flex>
-        </Flex>
-        <Flex alignItems="center" gap={5}>
-          <Flex
-            className="cursor-pointer hover:opacity-[.8]"
+    <div className="relative w-full h-[74px]">
+      <header className={classes + (isHeaderFloating ? floatingClasses : "")}>
+        <AirdropModal
+          isOpen={showTokenModal}
+          onClose={() => setShowTokenModal(false)}
+        />
+        <div className="flex gap-x-4 items-center">
+          <JumpIcon />
+          <JumpTextIcon />
+        </div>
+
+        <div className="flex gap-x-6">
+          {/* GetTestnet Tokens Button */}
+          <Button
+            inline
+            className="p-0 text-3.5 font-semibold leading-4 tracking-normal"
             onClick={() => setShowTokenModal(!showTokenModal)}
           >
-            <Text fontSize={20} fontWeight="800" letterSpacing="-0.03em">
-              Get Testnet Tokens
-            </Text>
-          </Flex>
+            Get Testnet Tokens
+          </Button>
 
-          {/* <Button isDisabled={true} onClick={toggleColorMode}>
-            <MoonIcon />
-          </Button> */}
-
+          {/* Wallet Button/Menu */}
           <Wallet />
 
-          <Flex
-            alignItems="center"
-            justifyContent="center"
-            className="block md:hidden"
+          {/* Dark/Light Switch Button */}
+          <Button
+            white
+            className="h-10 aspect-square p-0 hidden"
+            onClick={() => setStyleScheme(!isStyleSchemeDark)}
           >
-            <Button onClick={() => setIsOpen(true)}>
-              <MenuIcon />
-            </Button>
-          </Flex>
+            {!isStyleSchemeDark && <MoonIcon className="h-3 fill-purple" />}
+            {isStyleSchemeDark && <SunIcon className="h-3" />}
+          </Button>
+
+          {/* Mobile Hamburger Button */}
+          <Button white onClick={() => setIsOpen(true)} className="lg:hidden">
+            <Bars3Icon className="h-3.5" />
+          </Button>
 
           <MobileNav isOpen={isOpen} onClose={() => setIsOpen(!isOpen)} />
-        </Flex>
-      </Flex>
+        </div>
+      </header>
     </div>
   );
 }
