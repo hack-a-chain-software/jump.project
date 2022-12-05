930cbf18690aba72d5c322b0a059e906aea726a7
Lara Vieira <contato@laravieira.me>
2022-12-04 23:31:35 -0300

    fix(web): Styled WarningBar.component

--- packages/web/src/components/WarningBar/WarningBar.component.tsx
+++ packages/web/src/components/WarningBar/WarningBar.component.tsx
@@ -4,10 +4,10 @@ import { ExclamationCircleIcon } from "@heroicons/react/24/outline";
 function WarningBar(props: HTMLAttributes<HTMLDivElement>) {
   return (
     <div
-      className={`flex items-center rounded-full bg-[03A9F4]/10 py-[5px] px-4 w-full ${props.className}`}
+      className={`flex items-center rounded-sm bg-[#03A9F4]/10 py-2.5 px-4 gap-2.5 border-[#03A9F4]/50 border-[1px] w-full ${props.className}`}
     >
-      <ExclamationCircleIcon className="h-8 w-8" />
-      <p className="text-3.5 font-extrabold leading-3.5 tracking-tight">
+      <ExclamationCircleIcon className="h-6 w-6 stroke-[#03A9F4]" />
+      <p className="text-3.5 font-medium leading-3.5 tracking-normal">
         {props.children}
       </p>
     </div>
